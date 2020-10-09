6adea0d4b44fcf01c4089b55ade79d57360ac47c
mammo0 <marc.ammon@hotmail.de>
2020-10-09 12:54:43 +0200

    Revert "removed python-prctl, since it doesn't work with pyinstaller"
    This reverts commit 19d36dc1026e71b8fd07c8b4663af7797547f4d4.

--- nord_nm_gui.py
+++ nord_nm_gui.py
@@ -6,6 +6,7 @@ import os
 import requests
 import shutil
 import time
+import prctl
 import keyring
 import subprocess
 import configparser
@@ -1258,6 +1259,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     app_name = "NordVPN"
+    prctl.set_name(app_name)
+    prctl.set_proctitle(app_name)
     app = QtWidgets.QApplication(sys.argv)
     ui = MainWindow()
     sys.exit(app.exec_())
